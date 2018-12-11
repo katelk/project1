01825a0ec7f1c7c5f99aa5e77a785ac21c7af43b
katelk <45261018+katelk@users.noreply.github.com>
2018-12-12 00:12:21 +0300

    Update Filters.py

--- Filters.py
+++ Filters.py
@@ -1,45 +1,77 @@
 from PIL import Image
-mode = int(input('mode:')) 
-image = Image.open("temp.jpg")
-draw = ImageDraw.Draw(image) 
-width = image.size[0] 
-height = image.size[1] 	
-pix = image.load()
--------------------------------
-import sys
+import os
+from PIL import Image
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import (QWidget, QHBoxLayout,
-    QLabel, QApplication)
+from PyQt5.QtWidgets import (QWidget, QLabel, QApplication)
+
 class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-
+ 
     def initUI(self):
-        hbox = QHBoxLayout(self)
-        pixmap = QPixmap("1.bmp")
+        self.setGeometry(300, 300, 260, 100)
+        self.setWindowTitle('Редактор фотографий')
+        
+        self.label1 = QLabel(self)
+        self.label1.setText("Добро пожаловать в редактор фотографий!")
+        self.label1.move(20, 8)
+        
+        self.label2 = QLabel(self)
+        self.label2.setText("Для начала работы введите имя файла:")
+        self.label2.move(25, 25)
+        
+        self.name_file_input = QLineEdit(self)
+        self.name_file_input.move(60, 45)
+        
+        self.btn = QPushButton('Приступить к работе', self)
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(68, 70)
+        self.btn.clicked.connect(self.action)
+        
+        
+    def action(self):
+        hbox = QVBoxLayout(self)
+        self.label1.deleteLater()
+        self.label2.deleteLater()
+        self.btn.deleteLater()
+        
+        self.name_file_input.deleteLater()
+        file = self.name_file_input.text()
+        im = Image.open(file)
+        im.thumbnail([500,500],Image.ANTIALIAS)
+        im.save("j.jpg")
+        pixmap = QPixmap("j.jpg")
+        #os.remove("j.jpg")
         
         self.lbl = QLabel(self)
         self.lbl.setPixmap(pixmap)
-        
-        btn = QPushButton('Заменить картинку', self)
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(self.action)
-    
         hbox.addWidget(self.lbl)
-        hbox.addWidget(btn)
-        self.setLayout(hbox)        
+        self.setLayout(hbox)
+        
+        self.btn2 = QPushButton('Черно-белый', self)
+        self.btn2.resize(self.btn2.sizeHint())
+        self.btn2.clicked.connect(self.black_whight)
+        hbox.addWidget(self.btn2)
+        
         self.move(300, 200)
-        self.setWindowTitle('Замена')
-        self.show()        
-
-    def action(self):
-        self.lbl.setPixmap(QPixmap("2.bmp"))       
-
+        self.show() 
+    def black_whight(self):
+        im2 = Image.open("j.jpg")
+        pixels = im2.load()
+        x, y = im2.size   
+        for i in range(x):  
+            for j in range(y):
+                r, g, b = pixels[i, j]
+                bw = (r + g + b) // 3
+                pixels[i, j] = bw, bw, bw
+        im2.save("j.jpg")
+        pixmap = QPixmap("j.jpg")
+        self.lbl.setPixmap(pixmap)
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
     sys.exit(app.exec())
-    ------------------------------------------------
+
